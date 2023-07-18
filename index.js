71c96b927eb86e4aec3f5a2a9e030625d6d363ef
robert neriko <nerikorobert@gmail.com>
2023-07-18 11:59:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(record) {
+    
+
+}
+function superbowlWin(array) {
+    const winningTeam = array.find((team) => team.result === "W");
+    return winningTeam ? winningTeam.year : undefined;
+  }
